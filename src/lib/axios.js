dc341ed8580313e17735c68d6106b9037af271c8
shubham Ranjan49 <shubhamranjansinha54321@gmail.com>
2025-12-31 18:25:24 +0530

    Update API endpoint from localhost to https://chat-back-end-ten.vercel.app/

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://chat-back-end-ten.vercel.app/api" : "https://chat-back-end-ten.vercel.app/api",
   withCredentials: true,
 });
